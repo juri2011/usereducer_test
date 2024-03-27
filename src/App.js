38cd9b7a36fedff4c1105bd89a8203751b89326b
오주리 <ojl98@naver.com>
2024-03-27 14:51:29 +0900

    useReduce 활용

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 import { useReducer, useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const[number, setNumber] = useState(1);
+  const [count, countDispatch] = useReducer(countReducer,0);
+  
+  function countReducer(oldCount, action){
+    if(action.type === 'UP'){
+      return oldCount+action.number;
+    }else if(action.type === 'DOWN'){
+      return oldCount-action.number;
+    }else if(action.type === 'RESET'){
+      return 0;
+    }
+  }
 
+  function changeNumber(event){
+    setNumber(Number(event.target.value));
+  }
   function down(){
-  setCount(count-1);
+    countDispatch({type:'DOWN',number: number});
   }
   function up(){
-  setCount(count+1);
+    countDispatch({type:'UP', number: number});
   }
   function reset(){
-  setCount(0);
+    countDispatch({type:'RESET', number:number});
   }
 
   return (
@@ -20,7 +33,7 @@ function App() {
       <input type="button" value="-" onClick={down}/>
       <input type="button" value="0" onClick={reset}/>
       <input type="button" value="+" onClick={up}/>
-      <input type="text" />
+      <input type="text" value={number} onChange={changeNumber}/>
       <span>{count}</span>
     </div>
   );
