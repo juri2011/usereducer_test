e89ac0de97775c7cfecb34ca51856c6487033a77
오주리 <ojl98@naver.com>
2024-03-27 14:29:59 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useReducer, useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function down(){
+  setCount(count-1);
+  }
+  function up(){
+  setCount(count+1);
+  }
+  function reset(){
+  setCount(0);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="button" value="-" onClick={down}/>
+      <input type="button" value="0" onClick={reset}/>
+      <input type="button" value="+" onClick={up}/>
+      <input type="text" />
+      <span>{count}</span>
     </div>
   );
 }
